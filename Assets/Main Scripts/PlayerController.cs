d6a3ef1b4d8542c9dbe4b30ebd0e13e0134b46b4
rokzidarn <rok.zidarn@gmail.com>
2016-12-29 12:37:46 +0100

    test run

---
+++ Assets/Main Scripts/PlayerController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	public float speed;
+
+	void FixedUpdate (){
+		float moveH = Input.GetAxis("Horizontal");
+		float moveV = Input.GetAxis("Vertical");
+
+		Vector3 movement = new Vector3 (moveH * speed,0.0f,moveV * speed);
+		Rigidbody rigidbody= GetComponent<Rigidbody>();
+		rigidbody.velocity = movement;
+	}
+}
